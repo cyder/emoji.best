07925a32e4610ec1218bb6392e1bbad1b06a86d6
Mori Atsushi <mori@cyder.tech>
2018-04-10 23:25:45 +0900

    [clear] リファクタ

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,7 +1,5 @@
 class Api::V1::BaseController < ApplicationController
-  rescue_from ActiveRecord::RecordInvalid, with: :render_bad_request
-
-  def render_bad_request(e)
+  rescue_from ActiveRecord::RecordInvalid do |e|
     errors = e.record.errors.details
     render template: "api/v1/errors/error", locals: { errors: errors }, status: :bad_request
   end
