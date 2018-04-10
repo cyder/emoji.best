b7bf341bf691322549f4a51badbcc6d089bc14c5
Mori Atsushi <at.suhi.tb@gmail.com>
2018-04-10 09:58:55 +0900

    [change] errorの形式を変更

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,24 +2,7 @@ class Api::V1::BaseController < ApplicationController
   rescue_from ActiveRecord::RecordInvalid, with: :render_bad_request
 
   def render_bad_request(e)
-    errors = e.record.errors.full_messages.map do |message|
-      { code: select_error_code(message), message: message }
-    end
+    errors = e.record.errors.details
     render template: "api/v1/errors/error", locals: { errors: errors }, status: :bad_request
   end
-
-  private
-
-    def select_error_code(message)
-      case message
-      when "Email has already been taken"
-        1
-      when "Password is too short (minimum is 6 characters)"
-        2
-      when "Password confirmation doesn't match Password"
-        3
-      else
-        0
-      end
-    end
 end
