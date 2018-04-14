f2dfc1dd440c3b25834708e142e945ff514c0bc8
tanishi <tanishi503@gmail.com>
2018-04-14 16:33:26 +0900

    [add] sign in

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,6 +4,15 @@ class Api::V1::UsersController < Api::V1::BaseController
     auto_login(@user)
   end
 
+  def sign_in
+    @user = login(params[:user][:email], params[:user][:password])
+
+    if @user
+      api_key = @user.activate
+      @access_token = api_key.access_token
+    end
+  end
+
   private
 
     def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resource :users, only: [:create]
+
+      namespace :users do
+        post :sign_in
+      end
     end
   end
 end
