de08742b956789e0ca1dd62b36cc5c9bccfc9850
tanishi <tanishi503@gmail.com>
2018-05-31 22:35:22 +0900

    Clean routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resource :users, only: [:create]
-
-      post "/users/sign_in", to: "user_sessions#create"
+      resource :users, only: [:create] do
+        post "sign_in", to: "user_sessions#create"
+      end
     end
   end
 end
