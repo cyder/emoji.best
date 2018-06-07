cd372a86fee4f9a4fba93b8f6fa2cfa445a6507d
Mori Atsushi <mori@cyder.tech>
2018-06-07 15:39:46 +0900

    [add] tokenと有効期限を生成するようにした

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -1,2 +1,14 @@
 class AccessToken < ApplicationRecord
+  before_create :generate_token, :set_expiration
+  belongs_to :user
+
+  private
+
+    def generate_token
+      self.token = "#{user.id}:#{SecureRandom.hex}"
+    end
+
+    def set_expiration
+      self.expires_at = DateTime.now + 30 #30日間有効
+    end
 end
