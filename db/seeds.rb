f4917017be152a362f5f664f3dcfe953b6c24473
Mori Atsushi <mori@cyder.tech>
2018-06-10 22:32:24 +0900

    [add] テストデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(
+  name: "test",
+  email: "test@test.com",
+  password: "password",
+  password_confirmation: "password",
+)
+
+Emoji.create(
+  user: user,
+  name: "sample_emoji1",
+  description: "sample",
+  original_image_url: "orizinal_image",
+  large_image_url: "large_image",
+  thumbnail_image_url: "thumbnail_image",
+  slack_image_url: "slack_image",
+)
+
+Emoji.create(
+  user: user,
+  name: "sample_emoji2",
+  description: "sample",
+  original_image_url: "orizinal_image",
+  large_image_url: "large_image",
+  thumbnail_image_url: "thumbnail_image",
+  slack_image_url: "slack_image",
+)
+
+Emoji.create(
+  user: user,
+  name: "sample_emoji3",
+  description: "sample",
+  original_image_url: "orizinal_image",
+  large_image_url: "large_image",
+  thumbnail_image_url: "thumbnail_image",
+  slack_image_url: "slack_image",
+)
