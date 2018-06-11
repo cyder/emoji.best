9b02e115ac12e4fe46eb0bf3ebae8c0de354f2b9
Mori Atsushi <mori@cyder.tech>
2018-06-11 21:29:11 +0900

    [add] Uploaderを作成

---
+++ app/uploaders/emoji_uploader.rb
@@ -0,0 +1,23 @@
+class EmojiUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_whitelist
+    %w[jpg jpeg gif png]
+  end
+
+  def filename
+    "#{model.name}.#{file.extension}" if original_filename
+  end
+end
