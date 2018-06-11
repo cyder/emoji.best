d506be259f1d32da93f2d81d8adb3ae1d20ee639
Mori Atsushi <mori@cyder.tech>
2018-06-11 22:01:50 +0900

    [change] emojiテーブルの構造を変更
    画像を保存できるように

---
+++ db/migrate/20180611125543_change_image_column_to_emoji.rb
@@ -0,0 +1,9 @@
+class ChangeImageColumnToEmoji < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :emojis, :original_image_url, :string
+    remove_column :emojis, :large_image_url, :string
+    remove_column :emojis, :thumbnail_image_url, :string
+    remove_column :emojis, :slack_image_url, :string
+    add_column    :emojis, :image, :string, null: false
+  end
+end
