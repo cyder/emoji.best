7e51a0d800bfd5342b8cfc056bdc805495915a3c
Mori Atsushi <mori@cyder.tech>
2018-06-14 19:09:24 +0900

    [add] emojiの情報を追加

--- app/javascript/app/components/emoji.jsx
+++ app/javascript/app/components/emoji.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 import EmojiShape from './shapes/emoji';
 
-const Emoji = ({ name }) => (
-  <article>{name}</article>
+const Emoji = emoji => (
+  <article>
+    <img alt={emoji.name} src={emoji.images.thumb_url} />
+    <h1>:{emoji.name}:</h1>
+    <hr />
+    <span>by {emoji.user.name}</span>
+    <span>{emoji.number_of_downloaded}</span>
+  </article>
 );
 
 Emoji.propTypes = EmojiShape;
