49b4775e49ac5970560a190d94b6651555a1fe74
Mori Atsushi <mori@cyder.tech>
2018-06-15 01:20:45 +0900

    [add] ダウンロード数とアップロード数を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
 
   has_many :emojis, dependent: :destroy
   has_many :access_tokens, dependent: :destroy
+  has_many :download_logs, dependent: :nullify
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :name, presence: true
   validates :password, presence: true, length: { minimum: 6 }, confirmation: true
@@ -12,5 +13,13 @@ class User < ApplicationRecord
 
   def activate
     AccessToken.create(user: self)
+  end
+
+  def number_of_downloaded
+    download_logs.size
+  end
+
+  def number_of_uploaded
+    emojis.size
   end
 end
