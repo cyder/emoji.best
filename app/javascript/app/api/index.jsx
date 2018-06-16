1d0a4a6dacf701c17e864349c3b4917a9736ada2
Mori Atsushi <mori@cyder.tech>
2018-06-16 18:11:59 +0900

    検索用のapiをたたけるようにした

--- app/javascript/app/api/index.jsx
+++ app/javascript/app/api/index.jsx
@@ -1,7 +1,11 @@
 const COMMON_URL = 'api/v1/';
 const SEARCH = 'search';
 
-export default function searchEmojis() {
-  const path = `${COMMON_URL}${SEARCH}`;
+export default function searchEmojis(keyword = null) {
+  const params = new URLSearchParams();
+  if (keyword != null) params.set('keyword', keyword);
+
+  const path = `${COMMON_URL}${SEARCH}?${params.toString()}`;
+
   return fetch(path).then(response => response.json());
 }
