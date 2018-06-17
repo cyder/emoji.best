77d73c07aebda9aeeb421fba4c39d70ad21d7ab8
Mori Atsushi <mori@cyder.tech>
2018-06-17 20:20:47 +0900

    [add] popupの切り替えを実装

--- app/javascript/app/components/popup-manager.jsx
+++ app/javascript/app/components/popup-manager.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Background = styled.div`
+  display: ${props => (props.isShow ? 'block' : 'none')}
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.6);
+`;
+
+const Container = styled.div`
+  background-color: #ffffff;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  border-radius: 10px;
+`;
+
+const selectPopup = (show) => {
+  switch (show) {
+    case 'sign_up':
+      return 'sign_up';
+    case 'sign_in':
+      return 'sign_in';
+    default:
+      return null;
+  }
+};
 
 const PopupManager = ({ show }) => (
-  <div>test{show}</div>
+  <Background isShow={show !== null}>
+    <Container>
+      {selectPopup(show)}
+    </Container>
+  </Background>
 );
 
 PopupManager.propTypes = {
