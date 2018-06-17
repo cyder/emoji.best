4aed68778df3ebb1420ac9b28cfa8b4e83d87088
Mori Atsushi <mori@cyder.tech>
2018-06-17 19:27:45 +0900

    [add] headerのデザインを追加

--- app/javascript/app/components/header.jsx
+++ app/javascript/app/components/header.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import LogoImage from 'images/logo.png';
 
 import SearchForm from './search-form';
 
+const Container = styled.header`
+  color: #ffffff;
+  background-color: #2d2d2d;
+  padding: 60px 5%;
+`;
+
+const Title = styled.header`
+  text-align: center;
+`;
+
+const Logo = styled.img`
+  width: 360px;
+`;
+
+const SubTitle = styled.p`
+  text-align: center;
+  font-size: 1.5rem;
+  margin: 10px 0 30px;
+`;
+
 const Header = ({ searchEmojis }) => (
-  <header>
-    <h1>emoji.best</h1>
+  <Container>
+    <Title><Logo alt="emoji.best" src={LogoImage} /></Title>
+    <SubTitle>{'Let\'s share emojis!!'}</SubTitle>
     <SearchForm searchEmojis={searchEmojis} />
-  </header>
+  </Container>
 );
 
 Header.propTypes = {
