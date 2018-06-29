c6734146478907c822c9ce32c2ef8c7947da96c9
Mori Atsushi <mori@cyder.tech>
2018-06-29 23:26:46 +0900

    [add] sign in actionを呼び出せるようにした

--- app/javascript/app/containers/sign-in-popup.jsx
+++ app/javascript/app/containers/sign-in-popup.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -17,21 +17,47 @@ import {
   CloseButton,
 } from '../components/css/popup';
 
-const SignInPopup = ({ closePopup, showSignUpPopup }) => (
-  <Container>
-    <Title>Sign In</Title>
-    <Form>
-      <TextForm type="email" placeholder="Email" />
-      <TextForm type="password" placeholder="Password" />
-      <Button>Sign In</Button>
-    </Form>
-    <Message>
-      Not a member?
-      <SwitchButton onClick={showSignUpPopup}>Sign Up</SwitchButton>
-    </Message>
-    <CloseButton onClick={closePopup} />
-  </Container>
-);
+class SignInPopup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.submit = this.submit.bind(this);
+  }
+
+  submit() {
+    this.props.signIn(this.state.email, this.state.password);
+  }
+
+  render() {
+    return (
+      <Container>
+        <Title>Sign In</Title>
+        <Form>
+          <TextForm
+            type="email"
+            placeholder="Email"
+            onChange={e => this.setState({ email: e.target.value })}
+          />
+          <TextForm
+            type="password"
+            placeholder="Password"
+            onChange={e => this.setState({ password: e.target.value })}
+          />
+          <Button onClick={this.submit}>Sign In</Button>
+        </Form>
+        <Message>
+          Not a member?
+          <SwitchButton onClick={this.props.showSignUpPopup}>Sign Up</SwitchButton>
+        </Message>
+        <CloseButton onClick={this.props.closePopup} />
+      </Container>
+    );
+  }
+}
 
 function mapStateToProps(state) {
   return { user: state.user };
@@ -49,6 +75,7 @@ const SignInPopupContainer = connect(mapStateToProps, mapDispatchProps)(SignInPo
 SignInPopup.propTypes = {
   closePopup: PropTypes.func.isRequired,
   showSignUpPopup: PropTypes.func.isRequired,
+  signIn: PropTypes.func.isRequired,
 };
 
 export default SignInPopupContainer;
