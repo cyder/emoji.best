263286e4e07910ebc7b5fb01a0efb3fddafe36eb
Mori Atsushi <mori@cyder.tech>
2018-06-30 21:56:07 +0900

    [fix] ちゃんとpropsが反映されるようにした
    historyからrouterに変更

--- app/javascript/app/app.jsx
+++ app/javascript/app/app.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { configureUrlQuery } from 'react-url-query';
-import createHistory from 'history/createBrowserHistory';
-
+import { RouterToUrlQuery } from 'react-url-query';
+import { BrowserRouter as Router } from 'react-router-dom';
 import AppContainer from './containers/app';
 import store from './store';
 
 require('./components/css/main');
 require('./components/css/normalize');
 
-const history = createHistory();
-configureUrlQuery({ history });
-
 const App = () => (
   <Provider store={store}>
-    <AppContainer />
+    <Router>
+      <RouterToUrlQuery>
+        <AppContainer />
+      </RouterToUrlQuery>
+    </Router>
   </Provider>
 );
 
