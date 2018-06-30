9cbe7a067d24eb168a8f50f949aa7013df9bd4e1
Mori Atsushi <mori@cyder.tech>
2018-06-30 15:24:02 +0900

    [add] post methodを追加

--- app/javascript/app/api/index.jsx
+++ app/javascript/app/api/index.jsx
@@ -2,11 +2,30 @@ const COMMON_URL = 'api/v1/';
 const SEARCH = 'search';
 const DOWNLOAD = 'download';
 
+const csrfToken = document.querySelector('meta[name=csrf-token]').content;
+
 function get(path, data = null) {
   const params = new URLSearchParams(data);
   const uri = `${path}?${params.toString()}`;
 
   return fetch(uri).then(response => response.json());
+}
+
+function post(path, data) {
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    'X-CSRF-Token': csrfToken,
+  };
+  const body = JSON.stringify(data);
+  const params = {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers,
+    body,
+  };
+
+  return fetch(path, { ...params }).then(response => response.json());
 }
 
 export function searchEmojis(order, keyword = null) {
