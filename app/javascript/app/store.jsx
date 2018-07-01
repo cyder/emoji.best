d4d10f2aa708049c80429064df9829b40bc60737
Mori Atsushi <mori@cyder.tech>
2018-07-02 00:01:04 +0900

    [add] persistの設定を追加

--- app/javascript/app/store.jsx
+++ app/javascript/app/store.jsx
@@ -1,13 +1,23 @@
 import 'babel-polyfill';
 import { createStore, applyMiddleware } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './reducers';
 import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = createStore(persistedReducer, applyMiddleware(sagaMiddleware));
 
 sagaMiddleware.run(rootSaga);
 
 export default store;
+export const persistor = persistStore(store);
