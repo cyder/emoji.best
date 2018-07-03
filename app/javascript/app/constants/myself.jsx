748dbe6488074cd5c5ccc6d33c1811c1910cb95c
Mori Atsushi <mori@cyder.tech>
2018-07-03 23:40:12 +0900

    [add] signup用のactionを作成

--- app/javascript/app/constants/myself.jsx
+++ app/javascript/app/constants/myself.jsx
@@ -1,5 +1,7 @@
 export const SIGNIN = 'myself/SIGNIN';
+export const SIGNUP = 'myself/SIGNUP';
 export const SUCCESS_SIGNIN = 'myself/SUCCESS_SIGNIN';
+export const SUCCESS_SIGNUP = 'myself/SUCCESS_SIGNUP';
 
 export const STATUS = {
   SIGNOUT: 'signout',
