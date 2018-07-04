f936d6a9ee39ec09127d842e6240458a77465292
Mori Atsushi <mori@cyder.tech>
2018-07-04 12:58:06 +0900

    [fix] actionを呼び出せるようにした

--- app/javascript/app/sagas/myself.jsx
+++ app/javascript/app/sagas/myself.jsx
@@ -2,27 +2,39 @@ import 'babel-polyfill';
 import { takeEvery, put } from 'redux-saga/effects';
 
 import { SIGNIN, SIGNUP } from '../constants/myself';
-import { successSignIn, successSignUp } from '../actions/myself';
+import {
+  successSignIn,
+  successSignUp,
+  failedSignIn,
+  failedSignUp,
+} from '../actions/myself';
 import { closePopup } from '../actions/popup-manager';
 import { POPUP } from '../constants/popup-manager';
 import { signIn, signUp } from '../api';
 
 function* sageSignIn(action) {
-  yield signIn(action.email, action.password)
-    .then((json) => {
-      put(successSignIn(json.user, json.access_token));
-      put(closePopup(POPUP.SIGN_IN));
-    })
-    .catch(() => {});
+  try {
+    const json = yield signIn(action.email, action.password);
+    yield put(successSignIn(json.user, json.access_token));
+    yield put(closePopup(POPUP.SIGN_IN));
+  } catch (status) {
+    yield put(failedSignIn(status));
+  }
 }
 
 function* sageSignUp(action) {
-  yield signUp(action.name, action.email, action.password, action.passwordConfirm)
-    .then((json) => {
-      put(successSignUp(json.user, json.access_token));
-      put(closePopup(POPUP.SIGN_UP));
-    })
-    .catch(() => {});
+  try {
+    const json = yield yield signUp(
+      action.name,
+      action.email,
+      action.password,
+      action.passwordConfirm,
+    );
+    yield put(successSignUp(json.user, json.access_token));
+    yield put(closePopup(POPUP.SIGN_UP));
+  } catch (status) {
+    yield put(failedSignUp(status));
+  }
 }
 
 export default function* emojisSaga() {
