bcced2c05011c8eed3b9cd0605d4b9a0070d6f6c
Mori Atsushi <mori@cyder.tech>
2018-07-04 23:31:44 +0900

    [add] Logoがrandomで変わるようにした

--- app/javascript/app/components/header.jsx
+++ app/javascript/app/components/header.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
-import LogoImage from 'images/logo.png';
+import LogoImage1 from 'images/logo/logo1.png';
+import LogoImage2 from 'images/logo/logo2.png';
+import LogoImage3 from 'images/logo/logo3.png';
+import LogoImage4 from 'images/logo/logo4.png';
+import LogoImage5 from 'images/logo/logo5.png';
+import LogoImage6 from 'images/logo/logo6.png';
+import LogoImage7 from 'images/logo/logo7.png';
+import LogoImage8 from 'images/logo/logo8.png';
+import LogoImage9 from 'images/logo/logo9.png';
+import LogoImage10 from 'images/logo/logo10.png';
+import LogoImage11 from 'images/logo/logo11.png';
 
 import SearchForm from '../containers/search-form';
 import Navigation from '../containers/navigation';
@@ -26,13 +36,40 @@ const SubTitle = styled.p`
   margin: 10px 0 30px;
 `;
 
-const Header = () => (
-  <Container>
-    <Title><Logo alt="emoji.best" src={LogoImage} /></Title>
-    <SubTitle>{'Let\'s share emojis!!'}</SubTitle>
-    <SearchForm />
-    <Navigation />
-  </Container>
-);
+const selectLogoImage = () => {
+  const LogoImages = [
+    LogoImage1,
+    LogoImage2,
+    LogoImage3,
+    LogoImage4,
+    LogoImage5,
+    LogoImage6,
+    LogoImage7,
+    LogoImage8,
+    LogoImage9,
+    LogoImage10,
+    LogoImage11,
+  ];
+  const id = Math.floor(Math.random() * LogoImages.length);
+  return LogoImages[id];
+};
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { logoImage: selectLogoImage() };
+  }
+
+  render() {
+    return (
+      <Container>
+        <Title><Logo alt="emoji.best" src={this.state.logoImage} /></Title>
+        <SubTitle>{'Let\'s share emojis!!'}</SubTitle>
+        <SearchForm />
+        <Navigation />
+      </Container>
+    );
+  }
+}
 
 export default Header;
