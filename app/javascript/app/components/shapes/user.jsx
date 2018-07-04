0ef3101e53cc6a53a2e1fb258aeaceac67d5b935
Mori Atsushi <mori@cyder.tech>
2018-07-04 11:20:07 +0900

    [add] popupを表示

--- app/javascript/app/components/shapes/user.jsx
+++ app/javascript/app/components/shapes/user.jsx
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 const UserShape = PropTypes.shape({
   id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
+  email: PropTypes.string,
+  number_of_uploaded: PropTypes.number,
+  number_of_downloaded: PropTypes.number,
 });
 
 export default UserShape;
--- app/javascript/app/containers/profile.jsx
+++ app/javascript/app/containers/profile.jsx
@@ -1,15 +1,85 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import faUser from '@fortawesome/fontawesome-free-solid/faUser';
 
-const Profile = () => (
-  <FontAwesomeIcon icon={faUser} />
+import UserShape from '../components/shapes/user';
+
+const Container = styled.span`
+  position: relative;
+`;
+
+const Icon = styled.span`
+`;
+
+const Popup = styled.div`
+  position: absolute;
+  min-width: 240px;
+  background-color: #ffffff;
+  right: 0;
+  color: #212121;
+  border-radius: 10px;
+  border: solid 3px #dfdfdf;
+  line-height: normal;
+`;
+
+const Name = styled.h2`
+  font-size: 1.2rem;
+  margin: 10px 10px 5px;
+`;
+
+const Email = styled.div`
+  font-size: 1rem;
+  margin: 5px 10px;
+  color: #dfdfdf;
+`;
+
+const Info = styled.div`
+  font-size: 1rem;
+  text-align: right;
+  margin: 5px 10px;
+`;
+
+const Number = styled.span`
+  font-size: 1.2rem;
+`;
+
+const Button = styled.button`
+  display: block;
+  background-color: transparent;
+  border: none;
+  font-size: 1rem;
+  font-weight: bold;
+  text-align: center;
+  margin: 10px auto;
+`;
+
+const Hr = styled.hr`
+  border: solid 1px #dfdfdf;
+`;
+
+const Profile = ({ user }) => (
+  <Container>
+    <Icon>
+      <FontAwesomeIcon icon={faUser} />
+    </Icon>
+    <Popup >
+      <Name>{ user.name }</Name>
+      <Email>{ user.email }</Email>
+      <Hr />
+      <Info><Number>{ user.number_of_uploaded }</Number> Upload</Info>
+      <Info><Number>{ user.number_of_downloaded }</Number> Download</Info>
+      <Hr />
+      <Button>Edit Profile</Button>
+      <Button>Sign Out</Button>
+    </Popup>
+  </Container>
 );
 
 function mapStateToProps(state) {
-  return { status: state.myself.status };
+  return { user: state.myself.user };
 }
 
 function mapDispatchProps(dispatch) {
@@ -19,6 +89,7 @@ function mapDispatchProps(dispatch) {
 const ProfileContainer = connect(mapStateToProps, mapDispatchProps)(Profile);
 
 Profile.propTypes = {
+  user: UserShape.isRequired,
 };
 
 export default ProfileContainer;
