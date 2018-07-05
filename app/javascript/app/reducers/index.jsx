f053319473ba5f441174eee8d73ec1bacb43dffc
Mori Atsushi <mori@cyder.tech>
2018-07-05 18:20:57 +0900

    [add] emoji情報取得用のaction追加

--- app/javascript/app/reducers/index.jsx
+++ app/javascript/app/reducers/index.jsx
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 
 import emojis from './emojis';
+import emoji from './emoji';
 import downloadCart from './download-cart';
 import popupManager from './popup-manager';
 import myself from './myself';
 
 export default combineReducers({
   emojis,
+  emoji,
   downloadCart,
   popupManager,
   myself,
