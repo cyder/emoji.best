738d12c88a837ae86d390bbae6b02c3fd5f28db3
Mori Atsushi <mori@cyder.tech>
2018-07-05 18:55:31 +0900

    [add] emoji情報を表示できるようにした

--- app/javascript/app/containers/emoji-detail.jsx
+++ app/javascript/app/containers/emoji-detail.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+
+import EmojiShape from '../components/shapes/emoji';
+import { STATUS } from '../constants/emoji';
+import * as EmojiActions from '../actions/emoji';
 
 const Background = styled.div`
   position: fixed;
@@ -20,20 +26,48 @@ const Container = styled.div`
   border-radius: 10px;
 `;
 
-const EmojiDetail = ({ match }) => (
-  <Background>
-    <Container>
-      { match.params.id }
-    </Container>
-  </Background>
-);
+class EmojiDetail extends Component {
+  componentWillMount() {
+    this.props.getEmoji(this.props.match.params.id);
+  }
+
+  render() {
+    const { status, emoji } = this.props.emoji;
+    return (
+      <Background>
+        <Container>
+          {
+            status === STATUS.SHOWING ? (<h1>{ emoji.name }</h1>) : null
+          }
+        </Container>
+      </Background>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { emoji: state.emoji };
+}
+
+function mapDispatchProps(dispatch) {
+  return bindActionCreators({
+    ...EmojiActions,
+  }, dispatch);
+}
 
 EmojiDetail.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
-      id: PropTypes.number.isRequired,
+      id: PropTypes.string.isRequired,
     }).isRequired,
   }).isRequired,
+  emoji: PropTypes.shape({
+    status: PropTypes.string.isRequired,
+    emoji: EmojiShape,
+  }).isRequired,
+  getEmoji: PropTypes.func.isRequired,
 };
 
-export default EmojiDetail;
+const EmojiDetailContainer = connect(mapStateToProps, mapDispatchProps)(EmojiDetail);
+
+export default EmojiDetailContainer;
