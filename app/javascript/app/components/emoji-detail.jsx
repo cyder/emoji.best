f7cd26615a7f3f1d7bd408da3b1d2c5c6bd8f2c1
Mori Atsushi <mori@cyder.tech>
2018-07-09 19:34:30 +0900

    [add] 詳細画面の右上にダウンロード用チェックボタンを追加

--- app/javascript/app/components/emoji-detail.jsx
+++ app/javascript/app/components/emoji-detail.jsx
@@ -69,7 +69,36 @@ const DownloadButton = styled.a`
   line-height: 38px;
 `;
 
-const EmojiPopup = ({ emoji, onClose }) => (
+const DownloadCheckBox = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: ${props => (props.isAddedToCart ? '#464646' : '#dfdfdf')};
+  border-radius: 25px;
+  position: absolute;
+  top: -20px;
+  right: -20px;
+
+  &::after {
+    display: block;
+    content: '';
+    position: absolute;
+    top: 16px;
+    left: 14px;
+    width: 18px;
+    height: 8px;
+    border-left: 5px solid #ffffff;
+    border-bottom: 5px solid #ffffff;
+    transform: rotate(-45deg);
+  }
+`;
+
+const EmojiPopup = ({
+  emoji,
+  onClose,
+  isAddedToCart,
+  deleteEmojiFromDownloadCart,
+  addEmojiToDownloadCart,
+}) => (
   <article>
     <Content>
       <Img alt={emoji.name} src={emoji.images.thumb_url} />
@@ -93,6 +122,12 @@ const EmojiPopup = ({ emoji, onClose }) => (
     >
       <FontAwesomeIcon icon={faDownload} /> download
     </DownloadButton>
+    <DownloadCheckBox
+      isAddedToCart={isAddedToCart}
+      onClick={() => (
+        isAddedToCart ? deleteEmojiFromDownloadCart(emoji) : addEmojiToDownloadCart(emoji)
+      )}
+    />
     <CloseButton onClick={onClose} />
   </article>
 );
@@ -100,6 +135,9 @@ const EmojiPopup = ({ emoji, onClose }) => (
 EmojiPopup.propTypes = {
   onClose: PropTypes.func.isRequired,
   emoji: EmojiShape.isRequired,
+  isAddedToCart: PropTypes.bool.isRequired,
+  addEmojiToDownloadCart: PropTypes.func.isRequired,
+  deleteEmojiFromDownloadCart: PropTypes.func.isRequired,
 };
 
 export default EmojiPopup;
--- app/javascript/app/containers/emoji-detail-popup.jsx
+++ app/javascript/app/containers/emoji-detail-popup.jsx
@@ -5,9 +5,11 @@ import PropTypes from 'prop-types';
 
 import EmojiDetail from '../components/emoji-detail';
 import EmojiShape from '../components/shapes/emoji';
+import DownloadCartShape from '../components/shapes/download-cart';
 import { Background, Container } from '../components/css/popup';
 import { STATUS } from '../constants/emoji';
 import * as EmojiActions from '../actions/emoji';
+import * as DownloadCartActions from '../actions/download-cart';
 
 class EmojiDetailPopup extends Component {
   componentWillMount() {
@@ -26,13 +28,22 @@ class EmojiDetailPopup extends Component {
 
   render() {
     const { status, emoji } = this.props.emoji;
+    const { list } = this.props.downloadCart;
+
     return (
       <Background isShow>
         <Container>
           {
             status === STATUS.SHOWING
-              ? (<EmojiDetail emoji={emoji} onClose={this.onClose} />)
-              : null
+              ? (
+                <EmojiDetail
+                  emoji={emoji}
+                  onClose={this.onClose}
+                  addEmojiToDownloadCart={this.props.addEmojiToDownloadCart}
+                  deleteEmojiFromDownloadCart={this.props.deleteEmojiFromDownloadCart}
+                  isAddedToCart={list.some(value => value.id === emoji.id)}
+                />
+              ) : null
           }
         </Container>
       </Background>
@@ -41,12 +52,16 @@ class EmojiDetailPopup extends Component {
 }
 
 function mapStateToProps(state) {
-  return { emoji: state.emoji };
+  return {
+    emoji: state.emoji,
+    downloadCart: state.downloadCart,
+  };
 }
 
 function mapDispatchProps(dispatch) {
   return bindActionCreators({
     ...EmojiActions,
+    ...DownloadCartActions,
   }, dispatch);
 }
 
@@ -68,6 +83,9 @@ EmojiDetailPopup.propTypes = {
     emoji: EmojiShape,
   }).isRequired,
   getEmoji: PropTypes.func.isRequired,
+  addEmojiToDownloadCart: PropTypes.func.isRequired,
+  deleteEmojiFromDownloadCart: PropTypes.func.isRequired,
+  downloadCart: DownloadCartShape.isRequired,
 };
 
 const EmojiDetailPopupContainer = connect(mapStateToProps, mapDispatchProps)(EmojiDetailPopup);
