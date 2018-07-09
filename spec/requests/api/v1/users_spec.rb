17c19da20e5b32273535fe149d7f0eb8e6cf4ece
Mori Atsushi <mori@cyder.tech>
2018-07-09 14:59:12 +0900

    [add] テストを追加

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -71,6 +71,91 @@ describe "POST /api/v1/users" do
   end
 end
 
+describe "PUT /api/v1/users" do
+  let(:user) { create(:user) }
+  let(:access_token) { create(:access_token, user: user) }
+  let(:headers) { { "Authorization" => access_token.token } }
+
+  context "with valid email" do
+    let(:params) { { user: { email: email } } }
+    let(:email) { "changed@email.com" }
+
+    it "returns a user", autodoc: true do
+      is_expected.to eq 200
+      json = JSON.parse(response.body)
+      expect(json["user"]["id"]).to eq user.id
+      expect(json["user"]["email"]).to eq email
+    end
+  end
+
+  context "with valid password" do
+    let(:params) { { user: { password: password, password_confirmation: password } } }
+    let(:password) { "new password" }
+
+    it "returns a user", autodoc: true do
+      is_expected.to eq 200
+      json = JSON.parse(response.body)
+      expect(json["user"]["id"]).to eq user.id
+      expect(json["user"]["email"]).to eq user.email
+    end
+  end
+
+  context "without password_confirmation" do
+    let(:params) { { user: { password: password } } }
+    let(:password) { "new password" }
+
+    it "returns a user" do
+      is_expected.to eq 400
+      json = JSON.parse(response.body)
+      expect(json["errors"]["password_confirmation"]).to be_present
+      expect(json["errors"]["password_confirmation"][0]["error"]).to eq "blank"
+    end
+  end
+
+  context "with invalid password_confirmation" do
+    let(:params) { { user: { password: password, password_confirmation: password_confirmation } } }
+    let(:password) { "new password" }
+    let(:password_confirmation) { "valid password" }
+
+    it "returns a user" do
+      is_expected.to eq 400
+      json = JSON.parse(response.body)
+      expect(json["errors"]["password_confirmation"]).to be_present
+      expect(json["errors"]["password_confirmation"][0]["error"]).to eq "confirmation"
+    end
+  end
+
+  context "without access token" do
+    let(:headers) { { "Authorization" => nil } }
+    it "return a error" do
+      is_expected.to eq 403
+      json = JSON.parse(response.body)
+      expect(json["errors"]["error"]).to be_present
+    end
+  end
+end
+
+describe "DELETE /api/v1/users" do
+  let(:user) { create(:user) }
+  let(:access_token) { create(:access_token, user: user) }
+  let(:headers) { { "Authorization" => access_token.token } }
+
+  context "with valid acess token" do
+    it "returns 200", autodoc: true do
+      is_expected.to eq 200
+    end
+  end
+
+  context "without access token" do
+    let(:headers) { { "Authorization" => nil } }
+    it "return a error" do
+      is_expected.to eq 403
+      json = JSON.parse(response.body)
+      expect(json["errors"]["error"]).to be_present
+    end
+  end
+end
+
 describe "GET /api/v1/users/:id" do
   let(:user) { create(:user) }
   let(:emoji) { build(:emoji, user: user) }
