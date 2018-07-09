6a380ab15aa5e10b9af8bf27370f59c92d06c19b
Mori Atsushi <mori@cyder.tech>
2018-07-09 14:32:34 +0900

    [add] ユーザ編集、削除用のAPIを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,11 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [:create, :show] do
-        collection do
-          post "sign_in", to: "user_sessions#create"
-          delete "sign_out", to: "user_sessions#destroy"
-        end
+      resource :users, only: [:create, :update, :destroy] do
+        post "sign_in", to: "user_sessions#create"
+        delete "sign_out", to: "user_sessions#destroy"
       end
+      resources :users, only: :show
       resources :search, only: [:index]
       resources :emojis, only: [:create, :show, :update, :destroy] do
         resources :tags, only: [:create, :destroy]
