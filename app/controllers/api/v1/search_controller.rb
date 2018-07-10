37c84278157e530ddc4584fb04fc02ceb1de7aed
Mori Atsushi <mori@cyder.tech>
2018-07-10 23:29:06 +0900

    [clear] リファクタ
    関数分けとコメント

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -9,16 +9,25 @@ class Api::V1::SearchController < Api::V1::BaseController
   DEFAULT_TARGET = Emoji::TARGET_METHOD[:all]
 
   def index
-    @num = params[:num]&.to_i || DEFAULT_PAGE_SIZE
-    @page = params[:page]&.to_i || DEFAULT_PAGE_NUM
-    @order = params[:order] || DEFAULT_ORDER
-    @target = params[:target] || DEFAULT_TARGET
-    @keyword = params[:keyword]&.gsub(/[[:blank:]]+/, " ")&.strip
+    check_params
+
+    # keywordが指定されており、最初のページのときは検索ログを残す
     if @keyword.present? && @page == DEFAULT_PAGE_NUM
       SearchLog.create!(user: current_user, keyword: @keyword, order: @order, target: @target)
     end
+
     items = @keyword.present? ? Emoji.search_with_target(@keyword, @target) : Emoji.all
     @total = items.size
     @emojis = items.order_emojis(@order).select_range(@page, @num).includes([:tags, :user])
   end
+
+  private
+
+    def check_params
+      @num = params[:num]&.to_i || DEFAULT_PAGE_SIZE
+      @page = params[:page]&.to_i || DEFAULT_PAGE_NUM
+      @order = params[:order] || DEFAULT_ORDER
+      @target = params[:target] || DEFAULT_TARGET
+      @keyword = params[:keyword]&.gsub(/[[:blank:]]+/, " ")&.strip
+    end
 end
