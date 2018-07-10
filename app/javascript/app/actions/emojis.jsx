11fda1cc474c76ac679cea5b79b8434417c069a9
Mori Atsushi <mori@cyder.tech>
2018-07-10 22:35:00 +0900

    [fix] 2ページ以降正しくtargetが指定されないバグを修正

--- app/javascript/app/actions/emojis.jsx
+++ app/javascript/app/actions/emojis.jsx
@@ -15,12 +15,13 @@ export function searchEmojis(keyword, order, target = null) {
   };
 }
 
-export function loadNextEmojis(page, keyword, order) {
+export function loadNextEmojis(page, keyword, order, target = null) {
   return {
     type: types.LOAD_NEXT,
     keyword,
     order,
     page,
+    target,
   };
 }
 
