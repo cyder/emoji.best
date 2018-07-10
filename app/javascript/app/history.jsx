953a67aad5d2aa274685192180df668d129f1680
Mori Atsushi <mori@cyder.tech>
2018-07-10 15:29:08 +0900

    [change] google analyticsの初期化

--- app/javascript/app/history.jsx
+++ app/javascript/app/history.jsx
@@ -1,5 +1,13 @@
 import createHistory from 'history/createBrowserHistory';
+import ReactGA from 'react-ga';
 
 const history = createHistory();
+
+ReactGA.initialize('UA-000000-01');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+history.listen((location) => {
+  ReactGA.pageview(location.pathname + location.search);
+});
 
 export default history;
