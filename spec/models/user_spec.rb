b31346e2d70218110927190b181315f979faa166
Mori Atsushi <mori@cyder.tech>
2018-07-11 21:24:24 +0900

    [add] testを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,14 @@ describe User do
     user = build(:user)
     expect(user).to be_valid
   end
+
+  context "with sns" do
+    let(:user) { build(:user_with_twitter) }
+    before { user.save validate: false }
+
+    it "can be updated." do
+      user.update(name: "updated name")
+      expect(user).to be_valid
+    end
+  end
 end
