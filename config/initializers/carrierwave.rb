1a47353decadb403872f6b7cb6f62627962b2e6d
Mori Atsushi <mori@cyder.tech>
2018-07-15 15:30:18 +0900

    [add] fogの設定を追加

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,14 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.storage = :fog
+    config.fog_credentials = {
+      provider: "AWS",
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: ENV["AWS_REGION"],
+    }
+    config.fog_public = true
+    config.fog_directory = ENV["AWS_S3_BUCKET"]
+    config.cache_storage = :fog
   end
 else
   CarrierWave.configure do |config|
