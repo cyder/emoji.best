c76316e99db5125981e5068960da6e7de0022f46
witalosk <shun_september@live.jp>
2018-08-05 19:16:57 +0900

    [remove] emojiのlargeサイズを削除

--- app/uploaders/emoji_uploader.rb
+++ app/uploaders/emoji_uploader.rb
@@ -1,10 +1,6 @@
 class EmojiUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  version :large do
-    process resize_to_limit: [512, 512]
-  end
-
   version :thumb do
     process resize_to_limit: [256, 256]
   end
