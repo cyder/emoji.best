15daf70a1bc0a8f8284f4669921a981b4fa509ca
Mori Atsushi <mori@cyder.tech>
2018-08-06 23:26:59 +0900

    [clear] upload buttonも統一化

--- app/javascript/app/containers/upload-popup.jsx
+++ app/javascript/app/containers/upload-popup.jsx
@@ -17,6 +17,7 @@ import {
   Title,
   CloseButton,
 } from '../components/css/popup';
+import FillButton from '../components/atoms/buttons/fill-button';
 
 const UploadContainer = styled(Container)`
   width: 80vw;
@@ -59,20 +60,9 @@ const UploadMessage = styled.p`
   margin: 0 0 10px;
 `;
 
-const UploadButton = styled.button`
-  display: block;
+const UploadButtonWrapper = styled.div`
   width: 240px;
-  height: 44px;
-  margin: auto;
-  color: #ffffff;
-  background-color: #464646;
-  font-size: 1.1rem;
-  border: solid 3px #dfdfdf;
-  border-radius: 25px;
-  font-weight: bold;
-  text-decoration: none;
-  text-align: center;
-  margin-bottom: 20px;
+  margin: 0 auto 20px;
 `;
 
 class UploadPopup extends Component {
@@ -151,7 +141,9 @@ class UploadPopup extends Component {
             }
           </Emojis>
           <UploadMessage>choose {emojis.length} emojis</UploadMessage>
-          <UploadButton onClick={this.onSubmit}>Upload</UploadButton>
+          <UploadButtonWrapper>
+            <FillButton label="Upload "onClick={this.onSubmit} />
+          </UploadButtonWrapper>
           <CloseButton onClick={this.onClose} />
         </UploadContainer>
         <PopupBackground onClose={this.onClose} />
