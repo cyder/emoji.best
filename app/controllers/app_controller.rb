d31ae8cba2474777e77d4693f1ba42b0a1f81a00
Mori Atsushi <mori@cyder.tech>
2018-08-07 17:54:47 +0900

    [add] urlによってtitle書き換え

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,4 +1,19 @@
 class AppController < ApplicationController
   def index
   end
+
+  def signin
+    @page_title = "Sign In"
+    render action: :index
+  end
+
+  def signup
+    @page_title = "Sign Up"
+    render action: :index
+  end
+
+  def upload
+    @page_title = "Upload"
+    render action: :index
+  end
 end
