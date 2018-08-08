35487e1d030a6e7fa3c0c408c6094d7d034a0201
Mori Atsushi <mori@cyder.tech>
2018-08-08 22:35:47 +0900

    [clear] リファクタ

--- app/javascript/app/components/atoms/head/head-title.jsx
+++ app/javascript/app/components/atoms/head/head-title.jsx
@@ -3,8 +3,9 @@ import { Helmet } from 'react-helmet';
 import PropTypes from 'prop-types';
 
 const HeadTitle = ({ pageTitle }) => {
+  const prefix = pageTitle ? `${pageTitle} | ` : '';
   const siteTitle = 'emoji.best - Let\'s share emojis!!';
-  const title = (pageTitle ? `${pageTitle} |` : '') + siteTitle;
+  const title = `${prefix}${siteTitle}`;
 
   return (
     <Helmet>
