5cafc3a32b088f196144f2c59b468120e0bc9104
Mori Atsushi <mori@cyder.tech>
2018-08-09 13:58:16 +0900

    [change] download linkの生成を別ファイルに移動させた

--- app/javascript/app/api/index.jsx
+++ app/javascript/app/api/index.jsx
@@ -2,17 +2,8 @@ import * as emoji from './emoji';
 import * as tag from './tag';
 import * as user from './user';
 
-const COMMON_URL = '/api/v1/';
-const DOWNLOAD = 'download';
-
 export default {
   ...emoji,
   ...tag,
   ...user,
 };
-
-export function downloadEmojisLink(emojis) {
-  const params = new URLSearchParams();
-  emojis.forEach(emoji => params.append('emojis[]', emoji.id));
-  return `${COMMON_URL}${DOWNLOAD}?${params.toString()}`;
-}
