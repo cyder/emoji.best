5fb21c3d31b6398f1c4a29c13ed0cb0d566b3da5
Mori Atsushi <mori@cyder.tech>
2018-08-09 11:54:05 +0900

    [clear] リファクタ
    to_meta_tagsに切り出した

--- app/controllers/emojis_controller.rb
+++ app/controllers/emojis_controller.rb
@@ -1,27 +1,14 @@
 class EmojisController < ApplicationController
   def show
+    set_meta_tags canonical: request.base_url + request.path
     emoji = Emoji.find_by(id: params[:id])
+
     if emoji.present?
-      change_meta_tags(emoji)
+      set_meta_tags emoji
     else
-      @page_title = "not found"
+      set_meta_tags title: "not found"
     end
 
     render template: "app/index"
   end
-
-  private
-
-    def change_meta_tags(emoji)
-      tags_name = emoji.tags.pluck(:name)
-      tags_description = tags_name.empty? ? "" : "(tags: #{tags_name.join(", ")})"
-      description = "Detail of \"#{emoji.name}\" custom Emoji. #{emoji.description} #{tags_description}"
-
-      set_meta_tags title: emoji.name,
-                    description: description,
-                    keywords: tags_name.join(", "),
-                    canonical: request.base_url + request.path,
-                    og: { image: emoji.image.ogp },
-                    twitter: { card: "summary" }
-    end
 end
